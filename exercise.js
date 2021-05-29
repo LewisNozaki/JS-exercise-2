d2b2b78e0c0c1501e92dc7cb780d8e9f210e8a3e
Lewis Nozaki <lhnozaki@outlook.com>
2021-05-28 18:14:32 -1000

    updates

--- exercise.js
+++ exercise.js
@@ -29,3 +29,11 @@ function isPangram(string){
   
   return (count === 0);
 }
+
+// Solution 2
+function isPangram(string){
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+    return string.indexOf(x) !== -1;
+  });
+}
